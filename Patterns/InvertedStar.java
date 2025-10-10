ea9e61256e605c437747161ac3a696f83ec2f5b0
nitinLogic <nitin.yadav21193@gmail.com>
2025-10-10 10:33:32 +0530

    Add new file

---
+++ Patterns/InvertedStar.java
@@ -0,0 +1,17 @@
+//This program prints inverted star pattern
+import java.util.*;
+
+public class InvertedStar {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);    
+        System.out.print("Enter length of pattern: ");  //Input pattern length from user
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
