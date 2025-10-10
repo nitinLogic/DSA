54a554dc15eddc469307a3bd59a3f8ed34e235f0
nitinLogic <nitin.yadav21193@gmail.com>
2025-10-10 18:49:20 +0530

    Add new file

---
+++ Patterns/HalfPyramid.java
@@ -0,0 +1,17 @@
+//This program prints half pyramid
+import java.util.*;
+
+public class HalfPyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);    
+        System.out.print("Enter length of pattern: ");  //Input pyramid length from user
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
