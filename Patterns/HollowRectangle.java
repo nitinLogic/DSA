33a54a1e1a9a2e16f2b0f136724853c431a2ee74
nitinLogic <nitin.yadav21193@gmail.com>
2025-10-10 19:20:18 +0530

    Add new file

---
+++ Patterns/HollowRectangle.java
@@ -0,0 +1,31 @@
+//This program prints a hollow rectangle
+import java.util.*;
+
+public class HollowRectangle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);    
+        System.out.print("Enter length of pattern: ");  //Input pattern length
+        int n = sc.nextInt();
+
+        System.out.print("Enter breadth of pattern: ");  //Input pattern breadth
+        int m = sc.nextInt();
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                
+                if(i == 1 || i == m)
+                    System.out.print("* ");
+                else{
+
+                    if(j == 1 || j == n)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+
+                }
+
+            }
+            System.out.print("\n");
+        }
+    }
+}
