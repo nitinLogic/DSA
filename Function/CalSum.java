9d52c7d2da3a7602f5ba5385421c1cc018a1d773
nitinLogic <nitin.yadav21193@gmail.com>
2025-10-12 19:33:08 +0530

    Add folder and file

---
+++ Function/CalSum.java
@@ -0,0 +1,20 @@
+//This code prints sum of two numbers
+import java.util.*;
+
+public class CalSum {
+    //Function to add two numbers
+    public static int calSum(int a, int b){
+        return (a + b);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter value of b: ");
+        int b = sc.nextInt();
+
+        System.out.println("a + b = " + calSum(a,b));
+    }
+}
+
