3af213b2e1f20861696c2d3a2e50915369807bb6
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-15 11:59:16 +0530

    Add new file

---
+++ String/AnagramStr.java
@@ -0,0 +1,31 @@
+//This code checks if two strings are anagrams of each other
+import java.util.*;
+
+public class AnagramStr {
+    public static boolean checkAnagram(String str1, String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if(str1.length() == str2.length()){
+            //Convert string to char array
+            char[] chArray1 = str1.toCharArray();
+            char[] chArray2 = str2.toCharArray();
+
+            //Sort the character array
+            Arrays.sort(chArray1);
+            Arrays.sort(chArray2);
+
+            //Compare character arrays
+            if(Arrays.equals(chArray1, chArray2))
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        String str1 = "eart", str2 = "heart";
+        System.out.println(checkAnagram(str1, str2));
+    }    
+}
