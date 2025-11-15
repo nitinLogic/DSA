81e19f681fe9198c20760ee63e8790e690e37dbd
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-15 09:33:07 +0530

    Add new file

---
+++ String/String_Builder.java
@@ -0,0 +1,13 @@
+//This code shows use of String Builder instead of strings
+import java.util.*;
+
+public class String_Builder {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
