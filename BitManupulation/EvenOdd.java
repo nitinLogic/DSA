ec1c5020702b84b28a065ee532d7cff199d4ee37
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-17 20:05:37 +0530

    Modify old file

--- BitManupulation/EvenOdd.java
+++ BitManupulation/EvenOdd.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class EvenOdd {
     public static void oddOrEven(int n){
         int bitMask = 1;
-        if((n & bitMask) == 1)
-            System.out.println("Odd");
-        else
+        if((n & bitMask) == 0)
             System.out.println("Even");
+        else
+            System.out.println("Odd");
     }
 
     public static void main(String args[]){
