53de678457ba7a5fcf743494a9c6db22d74c7d93
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-21 17:55:56 +0530

    Modify old file

--- Recursion/ReverseOrder.java
+++ Recursion/ReverseOrder.java
@@ -2,19 +2,33 @@
 import java.util.*;
 
 public class ReverseOrder {
-    public static void printReverse(int n){
+    public static void printDec(int n){
         if(n == 1){
             System.out.print("1 ");
             return;
         }
         System.out.print(n + " ");
-        printReverse(n - 1);
+        printDec(n - 1);
     }
+
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print("1 ");
+            return;
+        }
+        printInc(n - 1);
+        System.out.print(n + " ");
+    }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
 
-        printReverse(n);
+        System.out.print("Increasing Order: ");
+        printInc(n);
+        System.out.print("\nDeacreasing Order: ");
+        printDec(n);
+        
     }
 }
