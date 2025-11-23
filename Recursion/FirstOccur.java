1c0a37502c2f21345bef3a8154ab70c4e5bef32f
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-23 20:01:29 +0530

    Add new file

---
+++ Recursion/FirstOccur.java
@@ -0,0 +1,21 @@
+//This code prints index of first occurrence of given integer in an array
+import java.util.*;
+
+public class FirstOccur {
+    public static int first(int a[], int n, int i){
+        //Item not present in the array
+        if(i >= a.length)
+            return -1;
+
+        //Found
+        if(a[i] == n)
+            return i;
+        return first(a, n, i + 1);
+    }
+
+    public static void main(String args[]){
+        int a[] = {2, 4, 5, 6, 8, 9, 2, 5};
+        int n = 5;
+        System.out.println("n first occures at index: " + first(a, n, 0));
+    }
+}
