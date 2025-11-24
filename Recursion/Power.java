25d839ee3ba781a2852de2f87de0969760a8d96a
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-24 17:11:23 +0530

    Update old file

--- Recursion/Power.java
+++ Recursion/Power.java
@@ -7,8 +7,21 @@ public class Power{
             return 1;
         return x * pow(x, n - 1);
     }
+    public static int optimisePow(int x, int n){
+        if(n == 0)
+            return 1;
+        int halfPow = optimisePow(x, n / 2);
+        int ans = halfPow * halfPow;
+
+        if(n % 2 != 0){
+            ans = x * ans;
+        }
+
+        return ans;
+    }
     public static void main(String args[]){
         int x = 2, n = 30;
         System.out.println(pow(x, n));
+        System.out.println(optimisePow(x, n));
     }
 }
