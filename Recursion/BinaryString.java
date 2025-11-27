4e2ed87f4eb6dae84d4a9be58e3acdb306791c2a
nitinLogic <nitin.yadav21193@gmail.com>
2025-11-27 17:58:11 +0530

    Add new file

---
+++ Recursion/BinaryString.java
@@ -0,0 +1,22 @@
+//This code prints all binary strings of size n without consecutive zeroes
+import java.util.*;
+
+public class BinaryString {
+    public static void binString(int n, String str){
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+        binString(n - 1, str + '0');
+
+        if(str.length() == 0 || str.charAt(str.length() - 1) != '1')
+            binString(n - 1, str + '1');
+
+        return;
+    }
+    public static void main(String args[]){
+        int n = 4;
+        binString(n, new String(""));
+    }
+}
