ea36c13c838b6997f8754da771f8300042765b76
nitinLogic <nitin.yadav21193@gmail.com>
2026-01-12 18:48:15 +0530

    Add new file

---
+++ Queue/QueueJCF.java
@@ -0,0 +1,17 @@
+//This code implements Queue using Java Collection Framework
+import java.util.*;
+
+public class QueueJCF {
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();  //Or ArrayDeque
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
